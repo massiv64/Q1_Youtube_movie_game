33829c15149d452aae080af7d8915632fc84687b
Nic Higuera <damagecpl@gmail.com>
2016-03-17 00:08:21 -0700

    second commit, have player and a bit more HTML

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	var tag = document.createElement('script');
+
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	var player;
+	function onYouTubeIframeAPIReady() {
+	    player = new YT.Player('player', {
+			height: '390',
+			width: '640',
+			videoId: 'M7lc1UVf-VE',
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+	    });
+		
+
+	}
+	function onPlayerReady(event) {
+		event.target.playVideo();
+	}
+
+
+});
+
+
+
+
+
+
+
+
+
